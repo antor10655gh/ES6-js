66b9852bb8d84a06bf8dd9e37cf9b44e2cf7fa3e
antor10655gh <creativefreelancerantor@gmail.com>
2022-02-21 02:24:50 +0600

    add problem8.js and problem9.js file

---
+++ problem8.js
@@ -0,0 +1,4 @@
+const number = [ 10, 20, 30, 40, 50];
+
+const [one, two, three, four, five] = number;
+console.log(three);
---
+++ problem9.js
@@ -0,0 +1,7 @@
+sum = (num1, num2, num3=7) => {
+    const num = num1 + num2 + num3;
+    return num;
+}
+
+const result = sum(10,20);
+console.log(result);
