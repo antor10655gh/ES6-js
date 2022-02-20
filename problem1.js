76e947795069e5adc7534e449738cb7ba2876e64
antor10655gh <creativefreelancerantor@gmail.com>
2022-02-21 01:17:44 +0600

    add problem1 and problem2.js file

---
+++ problem1.js
@@ -0,0 +1,7 @@
+divideByFive = (num) => {
+    const x = num/5;
+    return x;
+}
+const result = divideByFive(25);
+console.log(result);
+
---
+++ problem2.js
@@ -0,0 +1,9 @@
+multiplyNumber = (num1, num2) => {
+    const x = num1 + 2;
+    const y = num2 + 2;
+    const z = x * y;
+    return z;
+}
+
+const result = multiplyNumber(10, 10);
+console.log(result);
